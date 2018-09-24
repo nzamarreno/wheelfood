5271ae64850f9c4c7f74f56f5868d7aa062f0175
Nicolas Zamarreno <zamarreno.nicolas@gmail.com>
2018-09-25 00:27:07 +0200

    Clean SASS

--- front/src/enums/endpoint.ts
+++ front/src/enums/endpoint.ts
@@ -3,7 +3,7 @@
 * Mac: http://localhost:1112
 * Windows: http://192.168.99.101 || $ docker-compose ip
 */
-const address = "http://192.168.99.101:1112"
+const address = "http://localhost:1112"
 
 export const EndPoint = {
     ADRESS: address
